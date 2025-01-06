c8c57215a9cec291425e7f6642fca8c4be1ed022
yuki <kyoiii0219@163.com>
2025-01-06 16:30:56 +0800

    achieve scroll by FlatList

--- components/todo/GoalListSection.tsx
+++ components/todo/GoalListSection.tsx
@@ -1,24 +1,34 @@
 import React from "react";
-import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
 
 export const GoalListSection = (
 	{goalList, handleRemoveGoal}: { goalList: { id: string, value: string }[], handleRemoveGoal: (id: string) => void }
 ) => {
 	return (
 		<View style={styles.goalList}>
-			<ScrollView>
-			{goalList.map((goal, index) => {
-				return (
-					<Pressable key={index} onPress={() => {
-							handleRemoveGoal(goal.id);
-						}}>
+			{/*<ScrollView>*/}
+			{/*{goalList.map((goal, index) => {*/}
+			{/*	return (*/}
+			{/*		<Pressable key={index} onPress={() => {*/}
+			{/*				handleRemoveGoal(goal.id);*/}
+			{/*			}}>*/}
+			{/*			<View style={styles.goalListItem}>*/}
+			{/*				<Text style={{color: '#081e79'}}>{goal.value}</Text>*/}
+			{/*			</View>*/}
+			{/*			</Pressable>*/}
+			{/*	);*/}
+			{/*})}*/}
+			{/*</ScrollView>*/}
+			<FlatList data={goalList} renderItem={({item}) =>
+				(
+					<Pressable onPress={() => handleRemoveGoal(item.id)}>
 						<View style={styles.goalListItem}>
-							<Text style={{color: '#081e79'}}>{goal.value}</Text>
+							<Text style={{color: '#081e79'}}>{item.value}</Text>
 						</View>
-						</Pressable>
-				);
-			})}
-			</ScrollView>
+					</Pressable>
+				)}
+								keyExtractor={(item) => item.id}
+			/>
 		</View>
 	);
 };
