788628bc08a7f1aff1269dd6c881a0c5a8c4c9ca
yuki <kyoiii0219@163.com>
2025-01-08 17:05:06 +0800

    achieve first, refactor later

--- components/project-2-guess-number/InGame.tsx
+++ components/project-2-guess-number/InGame.tsx
@@ -2,25 +2,45 @@ import { StyleSheet, Text, View } from "react-native";
 import { GameTitle } from "@/components/project-2-guess-number/components/GameTitle";
 import { OperationButtonGroup } from "@/components/project-2-guess-number/components/OperationButtonGroup";
 import { Colors } from "@/constants/Colors";
+import { useState } from "react";
 
 export const InGame = ({setPage}: { setPage: (page: string) => void }) => {
-	const guessNumber = 50;
+
+	const getRandomNumber = ({min, max}: { min: string, max: string }) =>
+		String(Math.floor(Math.random() * (Number(max) - Number(min) + 1)) + Number(min));
+
+	const [min, setMin] = useState('0');
+	const [max, setMax] = useState('100');
+
+	const [guessHistory, setGuessHistory] = useState<string[]>([getRandomNumber({min, max})]);
+
+	const handleLow = () => {
+		// currentGuess < goalNumber
+		setMin(String(Number(guessHistory[0]) + 1));
+		setGuessHistory([getRandomNumber({min: guessHistory[0], max}), ...guessHistory]);
+	}
+
+	const handleHigh = () => {
+		// currentGuess > goalNumber
+		setMax(String(Number(guessHistory[0]) - 1));
+		setGuessHistory([getRandomNumber({min, max: guessHistory[0]}), ...guessHistory]);
+	}
+
 	return (
 		<View>
 			<GameTitle title={'Let\'s Guess !'}/>
 			<View style={styles.container}>
 				<Text style={{fontWeight: 'bold', fontSize: 18}}>Is this your number ?</Text>
-				<Text style={{fontSize: 24}}>50</Text>
+				<Text style={{fontSize: 24}}>{guessHistory[0]}</Text>
 				<OperationButtonGroup
-					handleLeft={() => {
-					}}
-					textLeft={'- lower -'}
-					textRight={'+ higher +'}
-					handleRight={() => {
-					}}
+					handleLeft={handleLow}
+					textLeft={'too low'}
+					textRight={'too high'}
+					handleRight={handleHigh}
 					themeColor={Colors.blue['400']}
 				/>
-				{guessNumber === 50 && <Text style={{color: Colors.green['400'], fontSize: 18, position:'absolute', right:20, bottom:20}}>✅</Text>}
+				{guessHistory[0] === '53' && (<Text
+					style={{color: Colors.green['400'], fontSize: 18, position: 'absolute', right: 20, bottom: 20}}>✅</Text>)}
 			</View>
 
 		</View>
