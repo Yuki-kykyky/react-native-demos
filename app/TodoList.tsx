da890a4cd569813a9c919968b5d53bcf2b8e547a
yuki <kyoiii0219@163.com>
2025-01-15 17:10:08 +0800

    update drawer

--- app/TodoList.tsx
+++ app/TodoList.tsx
@@ -1,10 +1,8 @@
-import { ImageBackground, Pressable, StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet } from "react-native";
 import { useState } from "react";
 import { GoalModal } from "@/components/project-1-todo-list/GoalModal";
 import { OperationSection } from "@/components/project-1-todo-list/OperationSection";
 import { GoalListSection } from "@/components/project-1-todo-list/GoalListSection";
-import { useNavigation } from "@react-navigation/native";
-import { Ionicons } from "@expo/vector-icons";
 
 export const TodoList = () => {
 	const [isVisible, setIsVisible] = useState(false);
@@ -17,16 +15,13 @@ export const TodoList = () => {
 	const handleRemoveGoal = (id: string) => {
 		setGoalList(goalList.filter((goal) => goal.id !== id));
 	};
-	const navigation = useNavigation();
+
 	return (
 		<ImageBackground
 			imageStyle={{opacity: 0.8}}
 			source={require('@/assets/images/background/todo-list.jpg')}
 			style={styles.componentLayout}
 		>
-			<Pressable onPress={() => navigation.goBack()} style={{position: 'absolute', top: 20, left: 20}}>
-				<Ionicons name={'arrow-back'} size={24} color={'#fff'}/>
-			</Pressable>
 			<GoalModal
 				isVisible={isVisible}
 				setIsVisible={setIsVisible}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,17 +3,81 @@ import { ProjectList } from "./ProjectList";
 import { TodoList } from "./TodoList";
 import { GuessNumber } from "./GuessNumber";
 import { HomePage } from "@/app/HomePage";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from "@react-navigation/drawer";
+import { ImageBackground } from "react-native";
+import { useScreenSize } from "@/hooks/useScreenSize";
+import { Ionicons } from "@expo/vector-icons";
 
-const myDrawer = createDrawerNavigator()
+const Drawer = createDrawerNavigator()
+const CustomDrawer = (props: any) => {
+  return (
+    <ImageBackground
+      source={require('@/assets/images/background/drawer.jpg')}
+      style={{flex: 1}}
+    >
+      <DrawerContentScrollView {...props}>
+        <DrawerItemList {...props}/>
+      </DrawerContentScrollView>
+    </ImageBackground>
+  )
+}
 
 export default function RootLayout() {
+  const {isLandscape, isSmall} = useScreenSize();
+
   return (
-    <myDrawer.Navigator>
-      <myDrawer.Screen name="Home" component={HomePage}/>
-      <myDrawer.Screen name="ProjectList" component={ProjectList}/>
-      <myDrawer.Screen name="ProjectOne" component={TodoList}/>
-      <myDrawer.Screen name="ProjectTwo" component={GuessNumber}/>
-    </myDrawer.Navigator>
+    <Drawer.Navigator
+      drawerContent={CustomDrawer}
+      screenOptions={{
+        overlayColor: '#00000080',
+        drawerActiveBackgroundColor: '#3c4e9f',
+        drawerActiveTintColor: '#fff',
+        drawerLabelStyle: {
+          fontSize: 16,
+          fontStyle: 'italic',
+        },
+        drawerStyle: {
+          width: isSmall ? '70%' : 240,
+        },
+        drawerType: isLandscape ? 'permanent' : 'slide',
+      }}
+    >
+      <Drawer.Screen
+        name="Home"
+        component={HomePage}
+        options={{
+          title: 'Welcome',
+          drawerLabel: 'Welcome',
+          drawerIcon: (props) => (<Ionicons name="home" size={props.size} color={props.color}/>),
+        }}
+      />
+      <Drawer.Screen
+        name="ProjectList"
+        component={ProjectList}
+        options={{
+          title: 'All Projects\' infos',
+          drawerLabel: 'All Projects\' infos',
+          drawerIcon: (props) => (<Ionicons name="list" size={props.size} color={props.color}/>),
+        }}
+      />
+      <Drawer.Screen
+        name="ProjectOne"
+        component={TodoList}
+        options={{
+          title: 'Todo List',
+          drawerLabel: 'Todo List',
+          drawerIcon: (props) => (<Ionicons name="code" size={props.size} color={props.color}/>),
+        }}
+      />
+      <Drawer.Screen
+        name="ProjectTwo"
+        component={GuessNumber}
+        options={{
+          title: 'Guess Number',
+          drawerLabel: 'Guess Number',
+          drawerIcon: (props) => (<Ionicons name="repeat" size={props.size} color={props.color}/>),
+        }}
+      />
+    </Drawer.Navigator>
   );
 }
