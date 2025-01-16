e2daebb0576ef21bdaf08c1cf559cf48f21a73af
yuki <kyoiii0219@163.com>
2025-01-16 17:36:24 +0800

    update details

--- components/project-3-meals-gallery/MealDetails.tsx
+++ components/project-3-meals-gallery/MealDetails.tsx
@@ -1,8 +1,9 @@
-import { Image, Text, View } from "react-native";
+import { FlatList, Image, Text, View } from "react-native";
 import Meal from "@/models/meal";
 import { MEALS } from "@/data/dummy-data";
 import { AffordabilityColor, ComplexityColor } from "@/constants/Colors";
 import { EAffordability, EComplexity } from "@/constants/Types";
+import { Ionicons } from "@expo/vector-icons";
 
 export const MealDetails = ({route}) => {
 	const {category} = route.params;
@@ -11,62 +12,112 @@ export const MealDetails = ({route}) => {
 	return (
 		<View style={{
 			flex: 1,
-			backgroundColor: '#b68181',
+			backgroundColor: '#673636',
 		}}>
-			{eligibleMeals.map((meal: Meal, index: number) => (
-				<View
-					key={meal.id}
-					style={{
-						flex: 1,
-						alignItems: 'center',
-						justifyContent: 'flex-start',
-						gap: 16,
-					}}>
-					<Text
+			<FlatList
+				data={eligibleMeals}
+				style={{marginVertical: 24}}
+				renderItem={({item, index}) => (
+					<View
 						style={{
+							alignItems: 'center',
+							justifyContent: 'flex-start',
+							gap: 8,
+							marginVertical: 12,
+						}}>
+						<Text
+							style={{
+								width: '80%',
+								textAlign: 'center',
+								fontSize: 21,
+								paddingVertical: 4,
+								borderBottomColor: 'white',
+								borderBottomWidth: 2,
+								borderTopColor: 'white',
+								borderTopWidth: 2,
+								color: 'white',
+								fontWeight: 'bold',
+							}}>{index + 1}</Text>
+						<Image
+							source={{uri: item.imageUrl}}
+							style={{
+								width: 200,
+								aspectRatio: 1,
+								marginVertical: 8,
+							}}/>
+						<Text
+							style={{
+								fontWeight: 'bold',
+								fontSize: 24,
+								fontStyle: 'italic',
+								textAlign: 'center',
+								color: 'white',
+							}}>{item.title}</Text>
+						<View style={{flexDirection: 'row', gap: 8}}>
+							<Text
+								style={{
+									backgroundColor: ComplexityColor[item.complexity as EComplexity],
+									paddingHorizontal: 12,
+									paddingVertical: 4,
+									borderRadius: 18,
+									fontWeight: 'bold',
+									fontSize: 12,
+								}}>{item.complexity}</Text>
+							<Text
+								style={{
+									backgroundColor: AffordabilityColor[item.affordability as EAffordability],
+									paddingHorizontal: 12,
+									paddingVertical: 4,
+									borderRadius: 18,
+									fontWeight: 'bold',
+									fontSize: 12,
+								}}>{item.affordability}</Text>
+						</View>
+						<View style={{
 							width: '80%',
-							textAlign: 'center',
-							fontSize: 21,
-							paddingVertical: 4,
-							borderBottomColor: 'white',
-							borderBottomWidth: 2,
-							color: 'white',
-							fontWeight: 'bold',
-						}}>{index + 1}</Text>
-					<Image
-						source={{uri: meal.imageUrl}}
-						style={{
-							width: 200,
-							aspectRatio: 1,
-						}}/>
-					<Text
-						style={{
-							fontWeight: 'bold',
-							fontSize: 24,
-							fontStyle: 'italic',
-						}}>{meal.title}</Text>
-					<View style={{flexDirection: 'row', gap: 8}}>
-						<Text
-							style={{
-								backgroundColor: ComplexityColor[meal.complexity as EComplexity],
-								paddingHorizontal: 12,
-								paddingVertical: 4,
-								borderRadius: 18,
+							backgroundColor: '#310808',
+							padding: 12,
+							paddingBottom: 0,
+							borderRadius: 8,
+						}}>
+							<Text style={{
 								fontWeight: 'bold',
-								fontSize: 12,
-							}}>{meal.complexity}</Text>
-						<Text
-							style={{
-								backgroundColor: AffordabilityColor[meal.affordability as EAffordability],
-								paddingHorizontal: 12,
-								paddingVertical: 4,
-								borderRadius: 18,
-								fontWeight: 'bold',
-								fontSize: 12,
-							}}>{meal.affordability}</Text>
-					</View>
-				</View>
-			))}
+								textAlign: 'center',
+								color: 'white',
+								borderBottomWidth: 2,
+								borderBottomColor: 'white',
+								marginBottom: 12,
+							}}>All we need are</Text>
+							<FlatList
+								numColumns={2}
+								columnWrapperStyle={{gap: 12}}
+								data={item.ingredients}
+								renderItem={({item}) => (
+									<View style={{
+										flexDirection: 'row',
+										gap: 8,
+										width: '45%',
+										marginBottom: 12,
+										alignItems: 'baseline',
+									}}>
+										<Ionicons name='checkmark' size={12} color='white'/>
+										<Text style={{color: 'white', fontSize: 12, width: '90%'}}>{item}</Text>
+									</View>
+								)}/>
+						</View>
+						<View style={{width: '80%', paddingRight:32}}>
+							<Text style={{color: 'white', fontWeight: 'bold', fontSize: 18, textAlign:'center', marginBottom:12}}>Steps</Text>
+							<FlatList
+								style={{alignSelf: 'flex-start', borderRadius: 8}}
+								data={item.steps} renderItem={({item, index}) => (
+								<View style={{flexDirection: 'row', alignItems: 'baseline'}}>
+									<Text style={{color: 'white', fontWeight: 'bold'}}>{index + 1}. </Text>
+									<Text style={{color: 'white', textAlign:'justify', width:'98%'}}>{item}</Text>
+								</View>
+							)}/>
+						</View>
+
+					</View>)}/>
 		</View>
 	)
 }
