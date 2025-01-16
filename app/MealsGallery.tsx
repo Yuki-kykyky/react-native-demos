38e26545d3f5396fcb1c5f535bee83a555f14ef7
yuki <kyoiii0219@163.com>
2025-01-16 15:39:33 +0800

    import stack navigation

--- app/MealsGallery.tsx
+++ app/MealsGallery.tsx
@@ -1,29 +1,15 @@
-import { FlatList, StyleSheet, View } from "react-native";
-import { CATEGORIES } from "@/data/dummy-data";
-import { MealCard } from "@/components/project-3-meals-gallery/MealCard";
+import { createStackNavigator } from "@react-navigation/stack";
+import AllMeals from "@/components/project-3-meals-gallery/AllMeals";
 
+const Stack = createStackNavigator();
 
 export const MealsGallery = () => {
 
 	return (
-		<View style={styles.container}>
-			<FlatList
-				numColumns={2}
-				columnWrapperStyle={{justifyContent: 'space-between', gap: 24}}
-				data={CATEGORIES}
-				keyExtractor={(item) => item.id}
-				renderItem={(category) => <MealCard item={category.item}/>}
-			/>
-		</View>
+		<Stack.Navigator screenOptions={{headerShown: false}}>
+			<Stack.Screen name="AllMeals" component={AllMeals}/>
+		</Stack.Navigator>
 	)
 };
 
 export default MealsGallery;
-
-const styles = StyleSheet.create({
-	container: {
-		backgroundColor: '#b68181',
-		padding: '5%',
-		flex: 1,
-	}
-});
---
+++ components/project-3-meals-gallery/AllMeals.tsx
@@ -0,0 +1,29 @@
+import { FlatList, StyleSheet, View } from "react-native";
+import { CATEGORIES } from "@/data/dummy-data";
+import { MealCard } from "@/components/project-3-meals-gallery/MealCard";
+
+
+export const AllMeals = () => {
+
+	return (
+		<View style={styles.container}>
+			<FlatList
+				numColumns={2}
+				columnWrapperStyle={{justifyContent: 'space-between', gap: 24}}
+				data={CATEGORIES}
+				keyExtractor={(item) => item.id}
+				renderItem={(category) => <MealCard item={category.item}/>}
+			/>
+		</View>
+	)
+};
+
+export default AllMeals;
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#b68181',
+		padding: '5%',
+		flex: 1,
+	}
+});
