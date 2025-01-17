81713d0d667c0c83b843e154ccab631605b52aaa
yuki <kyoiii0219@163.com>
2025-01-17 15:47:52 +0800

    extract styles

--- components/project-4-accounts/accountList.tsx
+++ components/project-4-accounts/accountList.tsx
@@ -6,60 +6,27 @@ export const AccountList = ({route}: { route: any }) => {
 
 	return (
 		<View style={styles.container}>
-			<View style={{
-				height: 60,
-				width: '90%',
-				backgroundColor: '#3667b0',
-				marginVertical: 10,
-				borderRadius: 12,
-				borderWidth: 2,
-				borderColor: '#ffffff',
-				alignItems: 'center',
-				justifyContent: 'center'
-			}}>
-				<Text style={{
-					color: '#fff',
-					fontSize: 18
-				}}>{route.params.type}</Text>
+			<View style={styles.sumContainer}>
+				<Text style={{color: '#fff', fontSize: 18}}>{route.params.type}</Text>
 			</View>
-			<View
-				style={{
-					width: '90%',
-					borderRadius: 12,
-					alignItems: 'center',
-					justifyContent: 'center',
-					paddingBottom: 96,
-				}}
-			>
-				{BILLS.length ? (<FlatList
-					style={{width: '100%'}}
-					data={sortByDateDesc(BILLS)} renderItem={({item}) => {
-					return (
-						<View style={{
-							paddingHorizontal: 24,
-							paddingVertical: 8,
-							marginVertical: 4,
-							borderRadius: 12,
-							backgroundColor: '#ffffff',
-						}}>
-							<View style={{
-								flexDirection: 'row',
-								justifyContent: 'space-between',
-								marginBottom: 8,
-							}}>
-								<Text style={{fontWeight: 'bold'}}>{item.title}</Text>
-								<Text>$ {item.amount.toFixed(2)}</Text>
-							</View>
-							<Text>{getDate(item.date)}</Text>
-						</View>
-					)
-				}}/>) : (
+			<View style={styles.billList}>
+				{BILLS.length ? (
+					<FlatList
+						style={{width: '100%'}}
+						data={sortByDateDesc(BILLS)}
+						renderItem={({item}) =>
+							(<View style={styles.billItem}>
+								<View style={styles.itemFirstRow}>
+									<Text style={{fontWeight: 'bold'}}>{item.title}</Text>
+									<Text>$ {item.amount.toFixed(2)}</Text>
+								</View>
+								<Text>{getDate(item.date)}</Text>
+							</View>)}/>) : (
 					<View>
 						<Text>No bills</Text>
 					</View>
 				)}
 			</View>
-
 		</View>
 	)
 }
@@ -69,5 +36,35 @@ const styles = StyleSheet.create({
 		flex: 1,
 		alignItems: 'center',
 		backgroundColor: '#7ba1e0',
+	},
+	sumContainer: {
+		height: 60,
+		width: '90%',
+		backgroundColor: '#3667b0',
+		marginVertical: 10,
+		borderRadius: 12,
+		borderWidth: 2,
+		borderColor: '#ffffff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	billList: {
+		width: '90%',
+		borderRadius: 12,
+		alignItems: 'center',
+		justifyContent: 'center',
+		paddingBottom: 96,
+	},
+	billItem: {
+		paddingHorizontal: 24,
+		paddingVertical: 8,
+		marginVertical: 4,
+		borderRadius: 12,
+		backgroundColor: '#ffffff',
+	},
+	itemFirstRow: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		marginBottom: 8,
 	}
 });
